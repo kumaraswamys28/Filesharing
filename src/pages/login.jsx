46a887f497b91c6dc830a1cde2697702327982cd
Manojkumbi <manojkumbi2803@gmail.com>
2025-06-30 21:06:44 +0530

    Auto commit #6 - 2025-06-30 21:06:44

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -144,7 +144,7 @@ const Login = () => {
 
           <div className="relative my-6">
             <div className="absolute inset-0 flex items-center">
-              <div className="w-full border-t border-primary">teststse</div>
+              <div className="w-full border-t border-primary"></div>
             </div>
             <div className="relative flex justify-center text-sm">
               <span className="px-4 bg-secondary text-tertiary">or</span>

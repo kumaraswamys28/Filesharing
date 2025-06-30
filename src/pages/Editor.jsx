08071cc567e45f14b3cad156279a527720616d0c
Manojkumbi <manojkumbi2803@gmail.com>
2025-06-30 21:34:00 +0530

    Auto commit #12 - 2025-06-30 21:34:00

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import Client from "../componets/Client";
 const Editor = () => {
-  const [clients, setClients] = useState([
+  const [clients, setClients] = useState([]);
+  useEffect(()=>{
+    setClients([
     { socketId: 1, username: "test" },
     { socketId: 1, username: "test" },
-    {socketId:1,username:'test'}
-  ]);
+    { socketId: 1, username: "test" },
+  ])
+  },[])
   return (
     <div className="min-h-[100vh] bg-primary flex pt-16 justify-center">
       <div className="w-full max-w-md">
@@ -13,7 +16,11 @@ const Editor = () => {
           this is aside
           <div className="aside-inner">
             <h1>this is inner</h1>
-            <div className="client-list"></div>
+            <div className="client-list">
+              {clients.map((client) => {
+                <Client key={client.socketId} username={client.username} />;
+              })}
+            </div>
           </div>
         </div>
         <div className="editorwrap text-secondary">

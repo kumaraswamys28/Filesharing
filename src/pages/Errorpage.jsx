2378d338adb795aaa97c40ae0b5c4028bf9ee0fa
Manojkumbi <manojkumbi2803@gmail.com>
2025-07-01 09:31:35 +0530

    Auto commit #2 - 2025-07-01 09:31:35

---
+++ src/pages/Errorpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Errorpage = () => {
+  return (
+    <div>
+          <h3 className="font-medium text-primary text-lg mb-3">this is an error page</h3>
+    </div>
+  )
+}
+
+export default Errorpage

55efd7542464673fee0346babf20afd8439f1c8b
Manojkumbi <manojkumbi2803@gmail.com>
2025-07-01 10:15:55 +0530

    Auto commit #22 - 2025-07-01 10:15:55

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 
 import Client from "../componets/Client";
